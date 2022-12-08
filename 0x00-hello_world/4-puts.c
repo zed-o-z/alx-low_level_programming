d8ce89beed81ae6593465b12b22a4b86271fa5fe
zed deju <zerubabel dejene>
2022-12-08 12:23:55 -0800

    fout

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
 /**
- *
- *  *main - Uses puts function to print a line
- *
- *   *
- *
- *    *Description: We are unable to use printf so must use put to print this string
- *
- *     *Return: Code should return a 0
- *
- *      */
+ * main - Uses puts function to print a line*
+ * Description: We are unable to use printf use put to print this string
+ * Return: Code should return a 0
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
