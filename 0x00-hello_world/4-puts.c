9f6277a871d31ce246ed2774b0c1c078997be5ad
zed deju <zerubabel dejene>
2022-12-08 12:05:44 -0800

    upto 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *
+ *  *main - Uses puts function to print a line
+ *
+ *   *
+ *
+ *    *Description: We are unable to use printf so must use put to print this string
+ *
+ *     *Return: Code should return a 0
+ *
+ *      */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+}
