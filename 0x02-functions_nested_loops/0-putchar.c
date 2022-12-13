dbf8e2f7b700e813d35fc914ec59282eb528da7d
zed deju <zerubabel dejene>
2022-12-13 10:42:23 -0800

    trying

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
-#include "_putchar.h"
+#include "main.h"
 /**
  * main - calls the function _putchar
- * Description: programs that prints _putchar, followed by a new line
+ * Description: programs that prints Holberton, followed by a new line
  * Return: 0
  */
-
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
-}
+		_putchar('H');
+			_putchar('o');
+				_putchar('l');
+					_putchar('b');
+						_putchar('e');
+							_putchar('c');
+								_putchar('h');
+									_putchar('a');
+										_putchar('r');
+											_putchar('\n');
+												return (0);
