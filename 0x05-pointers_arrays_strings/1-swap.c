085874894fccad7bebdf7106da227d2906ad43c5
zed deju <zerubabel dejene>
2022-12-19 12:00:18 -0800

    all pushed

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
-/**swap_int - swaps the values of two integers
+/**
+ * swap_int - swaps the values of two integers
  * @a: first integer
  * @b: second integer
- *    Return: nothing
+ * Return: nothing
  */
 void swap_int(int *a, int *b)
 
