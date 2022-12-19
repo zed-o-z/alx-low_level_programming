ebe165f7f9f2493ae5e5b0761eb6d07e2bee3eac
zed deju <zerubabel dejene>
2022-12-19 11:20:31 -0800

    pushed

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ *print_rev - Prints a string in reverse, with a new line
+ *@s: The string to reverse
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+
+{
+
+int len = _strlen(s) - 1;
+while (len >= 0)
+{
+_putchar(s[len]);
+len--;
+}
+_putchar('\n');
+}
+
+/**
+ *_strlen - returns the length of a string
+ * @s: the string whose length to return
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+
+{
+int i = 0;
+
+while (s[i] != '\0')
+i++;
+return (i);
+}
